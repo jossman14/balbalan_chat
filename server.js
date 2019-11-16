957dc4c6d45733023a8c8f11939539b080c832b7
Jossman <jossman@gmail.com>
2019-11-16 18:34:46 +0700

    update commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const http = require('http');
-const container = require('./container');
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const http = require("http");
+const container = require("./container");
 
 container.resolve(function() {
     const app = SetupExpress();
@@ -11,7 +11,13 @@ container.resolve(function() {
         const app = express();
         const server = http.createServer(app);
         server.listen(3000, function() {
-            console.log("Mendengarkan port 3000! (listen on port 3000)")
+            console.log("Mendengarkan port 3000! (listen on port 3000)");
         });
-    };
+    }
+
+    //Setup Router
+    const router = require("express-promise-router")();
+    users.SetRouting(router);
+
+    app.use(router);
 });
