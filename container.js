693ad6b417b9d7a66673b61e125dd393c634483b
Jossman <jossman@gmail.com>
2019-11-16 10:29:32 +0700

    update Commit

--- container.js
+++ container.js
@@ -4,17 +4,17 @@ const path = require('path');
 const container = dependable.container();
 
 const simpleDependencies = [
-    ['_', 'lodaash']
+    ['_', "lodash"]
 ];
 
 simpleDependencies.forEach(function(val) {
     container.register(val[0], function() {
-        return require(val[1]);
+        require(val[1]);
     })
 });
 
-container.load(path.join(__dirname, '/controllers'));
-container.load(path.join(__dirname, '/helpers'));
+container.load(path.join(__dirname, "./controllers"))
+container.load(path.join(__dirname, "./helpers"))
 
 container.register('container', function() {
     return container;
--- server.js
+++ server.js
@@ -3,3 +3,15 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const http = require('http');
 const container = require('./container');
+
+container.resolve(function() {
+    const app = SetupExpress();
+
+    function SetupExpress() {
+        const app = express();
+        const server = http.createServer(app);
+        server.listen(3000, function() {
+            console.log("listening port on 3000");
+        });
+    }
+});
