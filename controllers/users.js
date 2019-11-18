ffb70538318c25b0a9823ff1f49a716057852975
Jossman <jossman@gmail.com>
2019-11-18 09:30:02 +0700

    update commit

--- controllers/users.js
+++ controllers/users.js
@@ -3,13 +3,16 @@
 module.exports = function(_) {
     return {
         SetRouting: function(router) {
-            router.get('/', this.indexPage)
+            router.get('/', this.indexPage);
+            router.get('/signup', this.getSignUp);
         },
 
         indexPage: function(req, res) {
-            return res.render('index', {
-                test: 'this is a mnbmjnhjgjtest'
-            });
+            return res.render('index');
+        },
+
+        getSignUp: function(req, res) {
+            return res.render('signup')
         }
     }
 };
--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const http = require("http");
+const cookieParser = require("cookie-parser");
+const validator = require("express-validator");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const mongoose = require("mongoose");
+const flash = require("flash");
+const passport = require("passport");
+
 const container = require("./container");
 
 container.resolve(function(users) {
+    mongoose.Promise = global.Promise;
+    mongoose.connect("mongodb://localhost/balbalan", {
+        useMongoClient: true
+    });
     const app = SetupExpress();
 
     function SetupExpress() {
@@ -18,7 +30,7 @@ container.resolve(function(users) {
 
         //Setup Router
         // const router = require("express-promise-router");
-        const router = require('express-promise-router')();
+        const router = require("express-promise-router")();
         users.SetRouting(router);
 
         app.use(router);
@@ -26,6 +38,8 @@ container.resolve(function(users) {
 
     function ConfigureExpress(app) {
         app.use(express.static("public"));
+        app.use(validator());
+        app.use(cookieParser());
         app.set("view engine", "ejs");
         app.use(bodyParser.json());
         app.use(
@@ -33,5 +47,20 @@ container.resolve(function(users) {
                 extended: true
             })
         );
+
+        app.use(
+            session({
+                secret: "thisisasecretkey",
+                resave: true,
+                saveInitialized: true,
+                store: new MongoStore({
+                    mongooseConnection: mongoose.connection
+                })
+            })
+        );
+
+        app.use(flash());
+        app.use(passport.initialize());
+        app.use(passport.session());
     }
 });
